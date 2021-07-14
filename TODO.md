b4a6f4679ba9f39201337d29b9d7b6138d709184
igor <zalash1993@gmail.com>
2021-07-14 19:53:02 +0300

    todo

--- TODO.md
+++ TODO.md
@@ -3,11 +3,22 @@
 - SplashScreen loading icon (material logo on black background)
 
 ## Settings
-- Colors: grey and decent
 - Loading indicator
+- Show in settings hint: you can slide right to left to show settings button
+
 ## Logic
+Open menu in material by slide left to right
+Show settings button in App by slide right to left and hide after 5 seconds
+- Send to webview GPS coordinates, battery (GPS must run in background too)
+- Settings: instance (example: 'kids') => material.0.app.kids.battery (position)
+- Settings: send GPS and battery to ioBroker (Warning: APP will run in background)
+
 
 ## img
 wifi https://www.flaticon.com/free-icon/wifi_159599?term=wifi&page=1&position=7
 see https://www.flaticon.com/free-icon/eye-closeup_63786?term=see&page=1&position=15&page=1&position=15&related_id=63786&origin=search
 settings https://www.flaticon.com/free-icon/settings_2099174?term=settings&page=1&position=42&page=1&position=42&related_id=2099174&origin=search
+
+## How to build android app
+cd android
+./gradlew bundleRelease  
--- src/pages/WebViewComponent/index.js
+++ src/pages/WebViewComponent/index.js
@@ -4,18 +4,30 @@ import React, {
   useState,
   useEffect,
   useCallback,
-} from 'react';
-import { WebView } from 'react-native-webview';
-import styled from 'styled-components';
+} from "react";
+import { WebView } from "react-native-webview";
+import styled from "styled-components";
 // import WifiManager from "react-native-wifi-reborn";
 
-import { ContextWrapperCreate } from '../../components/ContextWrapper';
-import MyTabBar from '../../components/MyTabBar';
-import { Alert } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import { ScrollView } from 'react-native';
-import { RefreshControl } from 'react-native';
-import { useNetInfo } from "@react-native-community/netinfo";
+import { ContextWrapperCreate } from "../../components/ContextWrapper";
+import MyTabBar from "../../components/MyTabBar";
+import { Alert } from "react-native";
+import { useTranslation } from "react-i18next";
+import { ScrollView } from "react-native";
+import { RefreshControl } from "react-native";
+import { useNetInfo } from '@react-native-community/netinfo';
+import { Text } from "react-native";
+
+function buildLocalUrl(url) {
+  // check if material, admin, vis, echarts
+  if (url.includes(":3000") || url.includes("/material")) {
+    return url;
+  } else if (url.endsWith("/")) {
+    return url + "material/";
+  } else {
+    return url + "/material/";
+  }
+}
 
 const WebViewComponent = ({ navigation }) => {
   const { switchObj, emailObj, passwordObj, ssidObj, localObj } =
@@ -23,7 +35,7 @@ const WebViewComponent = ({ navigation }) => {
 
   const [useLocal, setUseLocal] = useState(true);
   const [uriLocal, setUriLocal] = useState(
-    `${localObj.localValue}?t=${Date.now()}`
+    `${buildLocalUrl(localObj.localValue)}?t=${Date.now()}`
   );
   const netInfo = useNetInfo();
 
@@ -31,7 +43,7 @@ const WebViewComponent = ({ navigation }) => {
   const { t } = useTranslation();
 
   useEffect(() => {
-    setUriLocal(`${localObj.localValue}?t=${Date.now()}`);
+    setUriLocal(`${buildLocalUrl(localObj.localValue)}?t=${Date.now()}`);
   }, [localObj.localValue]);
 
   useEffect(() => {
@@ -42,7 +54,7 @@ const WebViewComponent = ({ navigation }) => {
       passwordObj.passwordValue
     ) {
       if (
-        netInfo?.type === 'wifi' &&
+        netInfo?.type === "wifi" &&
         netInfo?.details?.ssid === ssidObj.ssidValue
       ) {
         setUseLocal(true);
@@ -61,13 +73,29 @@ const WebViewComponent = ({ navigation }) => {
     emailObj.emailValue,
     passwordObj.passwordValue,
   ]);
+
   const [refreshing, setRefreshing] = useState(false);
+  const [showSettings, setShowSettings] = useState(true);
   const onRefresh = useCallback(() => {
     if (refWebView.current) {
       refWebView.current.reload();
     }
     setTimeout(() => setRefreshing(false), 1500);
   }, []);
+
+  let warning;
+  if (
+    !localObj.localValue &&
+    (!emailObj.emailValue || !passwordObj.passwordValue)
+  ) {
+    // Show text. No local URL found
+    warning = t(
+      "iobroker.pro settings or local url not found! Please set it settings."
+    );
+  } else if (useLocal && !localObj.localValue) {
+    setUseLocal(false);
+  }
+
   return (
     <>
       <WrapperWebView
@@ -76,60 +104,81 @@ const WebViewComponent = ({ navigation }) => {
         }
         contentContainerStyle={{ flex: 1 }}
       >
-        <WebViewContent
-          ref={refWebView}
-          startInLoadingState
-          onError={(err) => {
-            Alert.alert(t('Error'), t('Redirect to iobroker.pro?'), [
-              {
-                text: t("No"),
-                style: 'cancel',
-              },
-              {
-                text: t('Yes'),
-                onPress: () => {
-                  const newURL = 'https://iobroker.pro';
-                  const redirectTo = `window.location = "${newURL}"`;
-                  refWebView.current?.injectJavaScript(redirectTo);
+        {!warning ? (
+          <WebViewContent
+            ref={refWebView}
+            startInLoadingState
+            onError={(err) => {
+              Alert.alert(t("Error"), t("Redirect to iobroker.pro?"), [
+                {
+                  text: t('No'),
+                  style: "cancel",
                 },
-              },
-            ]);
-          }}
-          renderLoading={() => {
-            setRefreshing(true);
-          }}
-          onLoadEnd={() => {
-            setRefreshing(false);
-          }}
-          onNavigationStateChange={(newNavState) => {
-            const { url } = newNavState;
-            if (!url) {
-              return;
-            }
-            if (url.includes('login?app=true')) {
-              const newURL =
-                'https://iobroker.pro/material/index.html?app=true';
-              const redirectTo = `window.location = "${newURL}"`;
-              refWebView.current?.injectJavaScript(redirectTo);
-            }
-          }}
-          source={
-            !useLocal
-              ? {
-                  uri: 'https://iobroker.pro/login?app=true',
-                  method: 'POST',
-                  body: `username=${emailObj.emailValue}&password=${passwordObj.passwordValue}`,
+                {
+                  text: t("Yes"),
+                  onPress: () => {
+                    const newURL = "https://iobroker.pro";
+                    const redirectTo = `window.location = "${newURL}"`;
+                    refWebView.current?.injectJavaScript(redirectTo);
+                  },
+                },
+              ]);
+            }}
+            onWebMessage={(e) => {
+              const data = e.nativeEvent.data;
+              if (data && data.includes('locationUpdated')) {
+                let newLoc = JSON.parse(data);
+                let url = newLoc.locationUpdated;
+                //handle the new URL or change anything needed on URL change
+                console.log(111121, refWebView.current, url);
+                if (
+                  (showSettings && url.includes("/chart/")) ||
+                  url.includes("/dialog/")
+                ) {
+                  setShowSettings(false);
+                } else if (!showSettings) {
+                  setShowSettings(true);
                 }
-              : {
-                  uri:
-                    (localObj.localValue && uriLocal) || "https://iobroker.pro",
-                }
-          }
-          on
-        />
+              }
+            }}
+            renderLoading={() => {
+              setRefreshing(true);
+            }}
+            onLoadEnd={() => {
+              setRefreshing(false);
+            }}
+            onNavigationStateChange={(newNavState) => {
+              const { url } = newNavState;
+              if (!url) {
+                return;
+              }
+
+              if (url.includes("login?app=true")) {
+                const newURL =
+                  "https://iobroker.pro/material/index.html?app=true";
+                const redirectTo = `window.location = "${newURL}"`;
+                refWebView.current?.injectJavaScript(redirectTo);
+              }
+            }}
+            source={
+              !useLocal
+                ? {
+                    uri: "https://iobroker.pro/login?app=true",
+                    method: "POST",
+                    body: `username=${emailObj.emailValue}&password=${passwordObj.passwordValue}`,
+                  }
+                : {
+                    uri: localObj.localValue && uriLocal,
+                  }
+            }
+            on
+          />
+        ) : (
+          <Text>{warning}</Text>
+        )}
       </WrapperWebView>
 
-      <MyTabBar navigation={navigation} />
+      {showSettings ? <MyTabBar navigation={navigation} /> : null}
     </>
   );
 };
